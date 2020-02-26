ec8f2a6d2ed78053f5e7d8035060bed340db7257
Iryna_Baranov <irina.panasiuk1@gmail.com>
2020-02-26 15:28:47 +0200

    webdrivermanager

--- src/test/java/core/WebDriverFactory.java
+++ src/test/java/core/WebDriverFactory.java
@@ -1,15 +1,15 @@
 package core;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class WebDriverFactory {
     private static WebDriver driver = null;
-    private static String projectPath = System.getProperty("user.dir");
 
     public static WebDriver initialize() {
         if (driver == null) {
-            System.setProperty("webdriver.chrome.driver", projectPath + "/src/test/java/drivers/chromedriver.exe");
+            WebDriverManager.chromedriver().setup();
             driver = new ChromeDriver();
         }
         return driver;
