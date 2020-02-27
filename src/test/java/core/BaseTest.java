3230429cbcfdff9af3908b2f32ad6b885992b79d
Iryna_Baranov <irina.panasiuk1@gmail.com>
2020-02-27 14:52:46 +0200

    ###url

--- src/test/java/core/BaseTest.java
+++ src/test/java/core/BaseTest.java
@@ -3,8 +3,6 @@ package core;
 import org.testng.ITestContext;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
-
 import java.util.Optional;
 
 import static core.WebDriverFactory.initialize;
