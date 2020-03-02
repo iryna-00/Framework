dc6ff3dc303c31b4fe481453730da514c594b8f5
Nazar_Gorodenchuk <Nazar_GOrodenchuk@epam.com>
2020-03-02 12:54:43 +0200

    business layer added

---
+++ src/java/pageObject/ContactUsPage.java
@@ -0,0 +1,11 @@
+package pageObject;
+
+import org.openqa.selenium.WebDriver;
+
+public class ContactUsPage extends Page {
+
+    public ContactUsPage(WebDriver webDriver) {
+        super(webDriver);
+        // TODO Auto-generated constructor stub
+    }
+}
---
+++ src/java/pageObject/Page.java
@@ -0,0 +1,17 @@
+package pageObject;
+
+import org.openqa.selenium.WebDriver;
+
+public class Page <T extends Page>{
+
+    protected WebDriver webDriver;
+
+
+    protected Page(WebDriver webDriver) {
+        this.webDriver = webDriver;
+    }
+
+//    explicit waits can be added
+}
+
+
