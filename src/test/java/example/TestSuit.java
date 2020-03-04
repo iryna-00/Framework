8c7967c3f51e36320e3264f7595c5305839ff007
Iryna_Baranov <irina.panasiuk1@gmail.com>
2020-03-04 16:54:11 +0200

    added parameters for test data

--- src/test/java/example/TestSuit.java
+++ src/test/java/example/TestSuit.java
@@ -1,6 +1,6 @@
 package example;
 
-import org.openqa.selenium.By;
+import org.testng.annotations.Parameters;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.interactions.Actions;
 import org.testng.Assert;
@@ -10,18 +10,21 @@ import pageObject.ContactUsPage;
 public class TestSuit extends TestBase {
 
     @Test(description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit")
-    public void textCaseExample() {
+    @Parameters({"firstName", "lastName", "email", "phone"})
+    public void textCaseExample(String firstName, String lastName, String email, String phone) {
         ContactUsPage contactUsPage = homePage.clickContactUsButton();
         contactUsPage.pageTitle();
         Assert.assertEquals(webDriver.getTitle(), contactUsPage.pageTitle());
-
         Actions builder = new Actions(webDriver);
         builder.contextClick(contactUsPage.selector()).sendKeys(Keys.ARROW_DOWN).sendKeys(Keys.ARROW_DOWN).sendKeys(Keys.RETURN).build().perform();
         Assert.assertTrue(Boolean.parseBoolean(contactUsPage.firstName().getAttribute("aria-required")));
-        contactUsPage.firstName().sendKeys("Iryna");
-        contactUsPage.lastName().sendKeys("Baranov");
-        contactUsPage.email().sendKeys("test@mail.com");
-        contactUsPage.phone().sendKeys("4324324234234");
+        Assert.assertTrue(Boolean.parseBoolean(contactUsPage.lastName().getAttribute("aria-required")));
+        Assert.assertTrue(Boolean.parseBoolean(contactUsPage.email().getAttribute("aria-required")));
+        Assert.assertTrue(Boolean.parseBoolean(contactUsPage.phone().getAttribute("aria-required")));
+        contactUsPage.firstName().sendKeys(firstName);
+        contactUsPage.lastName().sendKeys(lastName);
+        contactUsPage.email().sendKeys(email);
+        contactUsPage.phone().sendKeys(phone);
 
         contactUsPage.submitButton().click();
 
